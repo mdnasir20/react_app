183e925be1832a7042dda4738b64458b5ea88106
mdnasir20 <mdnasir.fi@gmail.com>
2022-08-26 12:33:47 +0300

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="Images/React-icon.svg.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project</h4>
+            
+        </nav>
+    )
+}
